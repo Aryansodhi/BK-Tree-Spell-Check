fc4cf1bbef65e0d216e445ae08c09213954e464c
Aryansodhi <141393151+Aryansodhi@users.noreply.github.com>
2024-12-08 18:15:28 +0530

    Update main.tsx

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BKTreeUI from './BKTreeUI'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
