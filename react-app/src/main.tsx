658979ff2fbc0527d2124f71e69d2e144c0f41ce
Aryansodhi <2023mcb1288@iitrpr.ac.in>
2024-12-08 17:51:17 +0530

    Update main.tsx

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from './App'
 import BKTreeUI from './BKTreeUI'
 import 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/bk-tree" element={<BKTreeUI />} />
+        <Route path="/" element={<BKTreeUI />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
